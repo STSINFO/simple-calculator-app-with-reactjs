719e3491d64e028733422a5f6446ce9e759e51b7
STSINFO <sts.info.io@gmail.com>
2024-11-30 01:09:06 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
 
 function App() {
+  const [result, setResult] = useState(0);
+  const inputRef = useRef("");
+  const resultRef = useRef("");
+
+  function add(e) {
+    e.preventDefault();
+    setResult((result) => result + Number(inputRef.current.value));
+  }
+  function minus(e) {
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value));
+  }
+  function times(e) {
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value));
+  }
+  function divide(e) {
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value));
+  }
+  function resetResult(e) {
+    e.preventDefault();
+    setResult(0);
+  }
+  function resetInput(e) {
+    e.preventDefault();
+    resultRef.current.value = " ";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p ref={resultRef}>{result}</p>
+      <input type="number" ref={inputRef} placeholder="Type something" />
+      <button onClick={add}>Add</button>
+      <button onClick={minus}>subtract</button>
+      <button onClick={times}>multiply</button>
+      <button onClick={divide}>divide</button>
+      <button onClick={resetResult}>reset result</button>
+      <button onClick={resetInput}>reset input</button>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,5 @@
+function Navbar() {
+  return <h1>Navebar</h1>;
+}
+
+export default Navbar;
